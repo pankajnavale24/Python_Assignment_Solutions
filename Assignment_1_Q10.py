847dc16139f4c68e90c00b23a17b9e4988bbf93f
Pankaj Navale <98072841+pankajnavale24@users.noreply.github.com>
2022-08-21 15:44:48 +0530

    Add files via upload

---
+++ Assignment_1_Q10.py
@@ -0,0 +1,5 @@
+#10. Write a python script to print values of three variables, each in a new line. Variable contains integer value
+a,b,c=1,2,3
+print(a)
+print(b)
+print(c)
---
+++ Assignment_1_Q9.py
@@ -0,0 +1,9 @@
+#9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+name="Pankaj"
+age=18
+qualification="B.tech"
+Years_of_Experiance=0
+print("Name=",name)
+print("age=",age)
+print("qualification=",qualification)
+print("Years_of_Experiance=",Years_of_Experiance)
